1501d01766e70b4cd331f2ae1a4c595a87fb595c
citrus327 <citrus327@outlook.com>
2023-06-30 14:20:13 +0800

    feat: add promise and async_await check

--- src/index.js
+++ src/index.js
@@ -1,30 +1,58 @@
-export var arrSpread = (suppress) => {
-  var arr1 = [1, 2, 3];
-  var tmp = [2, 3];
-  var arr2 = [1, ...tmp];
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    return true;
-  } else {
-    suppress && console.error("Does not support Array Spread Operator");
+export function arr_spread() {
+  try {
+    var arr1 = [1, 2, 3];
+    var tmp = [2, 3];
+    var arr2 = [1, ...tmp];
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+  } catch (e) {
+    console.error("Does not support Array Spread Operator");
     return false;
   }
-};
+}
 
-export var objSpread = (suppress) => {
-  var obj1 = { a: 1, b: 2, c: 3 };
-  var tmp = { b: 2, c: 3 };
-  var obj2 = { a: 1, ...tmp };
-  if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
-    return true;
-  } else {
-    suppress && console.error("Dose not support Object Spread Operator");
+export function obj_spread() {
+  try {
+    var obj1 = { a: 1, b: 2, c: 3 };
+    var tmp = { b: 2, c: 3 };
+    var obj2 = { a: 1, ...tmp };
+    return JSON.stringify(obj1) === JSON.stringify(obj2);
+  } catch (e) {
+    console.error("Dose not support Object Spread Operator");
     return false;
   }
-};
+}
+
+export function promise() {
+  if (
+    typeof Promise !== "undefined" &&
+    Promise.toString().indexOf("[native code]") !== -1
+  ) {
+    return true;
+  } else {
+    console.error("Dose not support Promise");
+    return false;
+  }
+}
+
+export function async_await() {
+  let supportAsync = true;
+
+  try {
+    eval("async () => {}");
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      supportAsync = false;
+    }
+  }
+
+  return supportAsync;
+}
 
 export function runAll() {
   return {
-    arrSpread: arrSpread(true),
-    objSpread: objSpread(true),
+    arrSpread: arr_spread(),
+    objSpread: obj_spread(),
+    Promise: promise(),
+    asyncAwait: async_await(),
   };
 }
